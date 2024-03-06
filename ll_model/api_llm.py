11e496ca92e324f9a0b6700449452ffdf7c7aae7
Aswin-00 <aswingirish25@gmail.com>
2024-03-06 15:41:18 +0530

    ok,ngrok

--- ll_model/api_llm.py
+++ ll_model/api_llm.py
@@ -33,5 +33,12 @@ def home(year):
 if __name__ == '__main__':
     # Authenticate ngrok (replace 'YOUR_AUTHTOKEN' with your actual ngrok authtoken)
 
+    ngrok.set_auth_token('2d8ZVeHli6ogXhNYDZkBOlRUkzO_7gxKeqj7trWhziRDHtHmR')
+
+    # Establish ngrok tunnel
+    public_url = ngrok.connect(port=5000)
+
+    print(public_url,)
+
     # Run the Flask app
     app.run(port=9800)
